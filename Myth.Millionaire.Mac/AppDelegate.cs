4b92ad941ed8181ef7c83e341fd8ed813a17d937
Myth <HalfLegend@live.com>
2018-04-13 14:07:07 +0800

    add mac project

---
+++ Myth.Millionaire.Mac/AppDelegate.cs
@@ -0,0 +1,23 @@
+﻿using AppKit;
+using Foundation;
+
+namespace Myth.Millionaire.Mac
+{
+    [Register("AppDelegate")]
+	public class AppDelegate : MvxAppDelegate
+    {
+        public AppDelegate()
+        {
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
---
+++ Myth.Millionaire.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Myth.Millionaire.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
